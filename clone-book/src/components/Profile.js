8c083333b16b4c31da1224a91d5d5b77816c0d37
Stephanie Podolak <StephPodolak@gmail.com>
2020-04-20 15:27:01 -0700

    add Profile component in Profile.js, nexted in App.js
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>

---
+++ clone-book/src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import About from "./About";
+
+function Profile() {
+  return (
+    <React.Fragment>
+      <Header />
+      <About />
+    </React.Fragment>
+  );
+}
+
+export default Profile;
