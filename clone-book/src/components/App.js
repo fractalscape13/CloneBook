62a634210a6227a7895c81de94c60fc58a6e0eab
Stephanie Podolak <StephPodolak@gmail.com>
2020-04-20 14:19:26 -0700

    create inital project directories. add navbar
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>

---
+++ clone-book/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "Navbar";
+
+function App(){
+  return (
+    <React.Fragment>
+      <Navbar />
+      <hr/>
+    </React.Fragment>
+  );
+}
+import Navbar from "./Navbar";
+
+export default App;
---
+++ clone-book/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <React.Fragment>
+      <h1>Home</h1>
+      <h1>Notifications</h1>
+      <h1>Messages</h1>
+      <input>Search</input>
+      <button>Tweet</button>
+    </React.Fragment>
+  );
+}
+
+export default Navbar;
