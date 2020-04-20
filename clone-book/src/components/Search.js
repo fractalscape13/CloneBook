ddd806081d8784b5c9ff7ab110e45cbd81d81f12
Stephanie Podolak <StephPodolak@gmail.com>
2020-04-20 15:28:15 -0700

    add Search component to Navbar.js and define in Search.js,
    Co-authored-by: Jose Amesquita <joseamesquita11@gmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>

---
+++ clone-book/src/components/Search.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Search() {
+  return (
+    <React.Fragment>
+      <form>
+        
+        <input defaultValue="Search"></input>
+        <button type="submit">Tweet</button>
+      </form>
+    </React.Fragment>
+  )
+}
+
+export default Search;
